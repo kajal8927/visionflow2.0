ea5a1c0a1c3423c25057c031eb72e57821b923e9
Kajal Yadav <www.kajal8927@gmail.com>
2026-05-05 20:27:29 +0530

    fix code

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,7 +38,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
